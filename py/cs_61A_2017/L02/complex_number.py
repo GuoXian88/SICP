86c3a53fd669e87fbfc19f78a4058e697eb01047
BeaverGuo <lalabeaver21@gmail.com>
2018-01-15 22:40:23 +0800

    add complex.py

--- py/cs_61A_2017/L02/complex_number.py
+++ py/cs_61A_2017/L02/complex_number.py
@@ -64,3 +64,23 @@ ri = ComplexRI(5, 12)
 ma = ComplexMA(2, pi/2)
 print(ri.magnitude)
 
+
+class Rational(Number):
+    def __init__(self, numer, denom):
+        g = gcd(numer, denom)
+        self.numer = numer // g
+        self.denom = denom // g
+
+    def __repr__(self):
+        return 'Rational({0}, {1})'.format(self.numer, self.denom)
+
+    def add(self, other):
+        nx, dx = self.numer, self.denom
+        ny, dy = other.numer, other.denom
+        return Rational(nx * dy + ny * dx, dx * dy)
+
+    def mul(self, other):
+        numer = self.numer * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
+
