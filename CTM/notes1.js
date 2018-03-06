d9d7a4a4fd546478e5b508dc0d8e9de5ec0df3f8
xianguo <xianguo@Ctrip.com>
2018-03-06 16:51:15 +0800

    ctm

--- CTM/notes1.js
+++ CTM/notes1.js
@@ -164,11 +164,250 @@ nested contexts.
 
 Advanced topics
 
- In the declarative model, functions are eager by default, i.e., function argu-
-ments are evaluated before the function body is executed. This is also called
+ In the declarative model, functions are eager by default, i.e., function arguments are evaluated before the function body is executed. This is also called
 strict evaluation.  The functional languages Scheme and Standard ML are
 strict.  There is another useful execution order, lazy evaluation, in which function arguments are evaluated only if their result is needed. 
 
 
 A table that remembers previous calls so that they can be
 avoided in the future is called a memoization table.
+
+??
+local  X  Y  Z  in
+  f(X  b)=f(a  Y)
+  f(Z  a)=Z
+  {Browse  [X  Y  Z]}
+end
+
+Entailment and disentailment checks (the == and \= operations)
+•  It returns the value true if the graphs starting from the nodes of X and Y
+have the same structure, i.e., all pairwise corresponding nodes have identical
+values or are the same node. We call this structure equality.
+•  It returns the value false if the graphs have diﬀerent structure, or some
+pairwise corresponding nodes have diﬀerent values.
+•  It blocks when it arrives at pairwise corresponding nodes that are diﬀerent,
+but at least one of them is unbound.
+
+
+Dynamic and static typing
+It is important for a language to be strongly-typed, i.e., to have a type system
+that is enforced by the language. 
+
+In a statically-typed language, on the other hand, all variable
+types are known at compile time. The type can be declared by the programmer or
+inferred by the compiler. When designing a language, one of the major decisions
+to make is whether the language is to be dynamically typed, statically typed, or
+some mixture of both.
+
+
+
+The basic principle is that static typing puts restrictions on
+what programs one can write, reducing expressiveness of the language in return
+for giving advantages such as improved error catching ability, eﬃciency, security,
+and partial program veriﬁcation. 
+
+•  Dynamic typing puts no restrictions on what programs one can write. To be
+precise, all syntactically-legal programs can be run. Some of these programs
+will raise exceptions, possibly due to type errors, which can be caught by
+an exception handler.  Dynamic typing gives the widest possible variety of
+programming techniques.  The increased ﬂexibility is felt quite strongly in
+practice. The programmer spends much less time adjusting the program to
+ﬁt the type system.
+•  Dynamic typing makes it a trivial matter to do separate compilation, i.e.,
+modules can be compiled without knowing anything about each other. This
+allows truly open programming, in which independently-written modules
+can come together at run time and interact with each other. It also makes
+program development scalable, i.e., extremely large programs can be divided
+into modules that can be compiled individually without recompiling other
+modules. This is harder to do with static typing because the type discipline
+must be enforced across module boundaries.
+•  Dynamic  typing  shortens  the  turnaround  time  between  an  idea  and  its
+implementation.  It enables an incremental development environment that
+is  part of the run-time  system.   It  allows  to test programs  or program
+fragments even when they are in an incomplete or inconsistent state.
+•  Static typing allows to catch more program errors at compile time.  The
+static type declarations are a partial speciﬁcation of the program, i.e., they
+specify part of the program’s behavior.  The compiler’s type checker veri-
+ﬁes that the program satisﬁes this partial speciﬁcation.  This can be quite
+powerful.   Modern static type systems can catch a surprising number of
+semantic errors.
+•  Static typing allows a more eﬃcient implementation. Since the compiler has
+more information about what values a variable can contain, it can choose a
+more eﬃcient representation. For example, if a variable is of boolean type,
+the compile can implement it with a single bit.   In a dynamically-typed
+language, the compiler cannot always deduce the type of a variable. When
+it cannot, then it usually has to allocate a full memory word, so that any
+possible value (or a pointer to a value) can be accommodated.
+•  Static typing can improve the security of a program. Secure ADTs can be
+constructed based solely on the protection oﬀered by the type system.
+
+
+Each approach has a role in practical application development.  Static typ-
+ing is recommended when the programming techniques are well-understood and
+when eﬃciency and correctness are paramount. Dynamic typing is recommended
+for rapid development and when programs must be as ﬂexible as possible, such
+as application prototypes, operating systems, and some artiﬁcial intelligence ap-
+plications.
+
+CH3 Declarative Programming Techniques
+
+Declarative programs are compositional.
+Reasoning about declarative programs is simple
+
+We start by looking more closely at the ﬁrst property.  Let us deﬁne a com-
+ponent as a precisely delimited program fragment with well-deﬁned inputs and
+outputs. A component can be deﬁned in terms of a set of simpler components. For
+example, in the declarative model a procedure is one kind of component.  The
+application program is the topmost component in a hierarchy of components.
+The hierarchy bottoms out in primitive components which are provided by the
+system.
+
+The basic technique for writing declarative programs is to consider the pro-
+gram as a set of recursive function deﬁnitions, using higher-orderness to simplify
+the program structure. 
+
+
+A recursive function is one whose deﬁnition body refers
+to the function itself, either directly or indirectly.  Direct recursion means that
+the function itself is used in the body. Indirect recursion means that the function
+refers to another function that directly or indirectly refers to the original function.
+Higher-orderness means that functions can have other functions as arguments and
+results. This ability underlies all the techniques for building abstractions that we
+will show in the book.  Higher-orderness can compensate somewhat for the lack
+of expressiveness of the declarative model, i.e., it makes it easy to code limited
+forms of concurrency and state in the declarative model.
+
+ Intuitively, it is programming by deﬁning the what (the
+results we want to achieve) without explaining the how (the algorithms, etc., need-
+ed to achieve the results).
+
+
+A descriptive declarativeness.如html, xml
+ A programmable declarativeness.
+There are two fundamentally diﬀerent ways to view programmable declarative-
+ness:
+•  A deﬁnitional view, where declarativeness is a property of the component
+implementation.  For example, programs written in the declarative model
+are guaranteed to be declarative, because of properties of the model.
+•  An observational view, where declarativeness is a property of the component
+interface. The observational view follows the principle of abstraction: that
+to use a component it is enough to know its speciﬁcation without knowing
+its implementation.  The component just has to behave declaratively, i.e.,
+as if it were independent, stateless, and deterministic, without necessarily
+being written in a declarative computation model.
+
+
+Two styles of deﬁnitional declarative programming have become particularly
+popular:  the functional and the logical.  In the functional style, we say that a
+component deﬁned as a mathematical function is declarative.   Functional lan-
+guages such as Haskell and Standard ML follow this approach.  In the logical
+style, we say that a component deﬁned as a logical relation is declarative.  Log-
+ic languages such as Prolog and Mercury follow this approach.  It is harder to
+formally manipulate functional or logical programs than descriptive programs,
+but they still follow simple algebraic laws.
+
+ Practically, declarative programs are very much like other programs: they
+require algorithms, data structures, structuring, and reasoning about the order of
+operations. This is because declarative languages can only use mathematics that
+can be implemented eﬃciently.  There is a trade-oﬀ between expressiveness and
+eﬃciency. Declarative programs are usually a lot longer than what a speciﬁcation
+could be. So the distinction between speciﬁcation and implementation still makes
+sense, even for declarative programs.
+
+
+ The basic principle is that a helper routine deﬁned only
+as an aid to deﬁne another routine should not be visible elsewhere.
+Putting them inside (Figures 3.6 and 3.7) lets them see the arguments of
+the main routines as external references, according to the lexical scoping
+rule (see Section 2.4.3).  Therefore, they need fewer arguments.  But each
+time the main routine is invoked, new helper routines are created.  This
+means that new procedure values are created.
+
+Putting them outside (Figures 3.4 and 3.5) means that the procedure values
+are created once and for all, for all calls to the main routine. But then the helper routines need more arguments so that the main routine can pass
+information to them.
+
+
+fun  {Iterate  S  IsDone  Transform}
+  if  {IsDone  S}  then  S
+  else  S1  in
+    S1={Transform  S}
+    {Iterate  S1  IsDone  Transform}
+  end
+end
+
+
+Recursive computation
+In programming, recursion
+occurs in two major ways:  in functions and in data types.  
+A data type is recursive if it is deﬁned in terms of
+itself.  For example, a list is deﬁned in terms of a smaller list.
+
+
+We saw that an iterative computation has a constant stack size.  This is not
+always the case for a recursive computation. Its stack size may grow as the input
+grows.  Sometimes this is unavoidable, e.g., when doing calculations with trees,
+as we will see later.  In other cases, it can be avoided.  An important part of
+declarative programming is to avoid a growing stack size whenever possible. 
+
+
+As before, we see that the stack grows by one statement per call. We summarize
+the diﬀerences between the two versions of the abstract machine:
+•  The environment-based abstract machine, deﬁned in Chapter 2, is faithful
+to the implementation on a real computer, which uses environments. How-
+ever, environments introduce an extra level of indirection, so they are hard
+to use for hand calculation.
+•  The substitution-based abstract machine is easier to use for hand calcu-
+lation,  because there are many fewer symbols to manipulate.   However,
+substitutions are costly to implement, so they are generally not used in a
+real implementation.
+
+ Converting a recursive to an iterative computation
+
+fun  {Fact  N}
+  if  N==0  then  1
+  elseif  N>0  then  N*{Fact  N-1}
+  else  raise  domainError  end
+  end
+end
+
+fun  {Fact  N}
+  fun  {FactIter  N  A}
+    if  N==0  then  A
+    elseif  N>0  then  {FactIter  N-1  A*N}
+    else  raise  domainError  end
+    end
+  end
+in
+  {FactIter  N  1}
+end
+
+The function that does the iteration, FactIter, has a second argument A. This
+argument is crucial; without it an iterative factorial is impossible.  The second
+argument is not apparent in the simple mathematical deﬁnition of factorial we
+used ﬁrst. We had to do some reasoning to bring it in.
+
+
+ Programming with recursion
+
+ Type notation
+ The list type is a subset of the record type.  There are other useful subsets of
+the record type, e.g., binary trees. 
+A list Xs is either nil or X|Xr where Xr is a list. Other subsets of the record
+type are also useful.  For example, a binary tree can be deﬁned as leaf(key:K
+value:V) or tree(key:K  value:V  left:LT  right:RT) where LT and RT are
+both binary trees. 
+
+<List T>   ::=   T ´|´ <List T>
+|   nil
+
+<BTree T>   ::=   tree(key: <Literal> value: T
+left: <BTree T> right: <BTree T>)
+|   leaf(key: <Literal> value: T)
+
+ Here are some cases where the notation is not good enough:
+•  The notation cannot deﬁne the positive integers, i.e., the subset of hInti
+whose elements are all greater than zero.
+•  The notation cannot deﬁne sets of partial values.  For example, diﬀerence
+lists cannot be deﬁned.
+
---
+++ CTM/notes2.js
@@ -0,0 +1,266 @@
+/*
+This section gives the
+basic techniques of programming with lists:
+•  Thinking recursively:  the basic approach is to solve a problem in terms of
+smaller versions of the problem.
+•  Converting recursive to iterative computations: naive list programs are often
+wasteful because their stack size grows with the input size.  We show how
+to use state transformations to make them practical.
+•  Correctness of iterative computations: a simple and powerful way to reason
+about iterative computations is by using state invariants.
+•  Constructing programs by following the type: a function that calculates with
+a given type almost always has a recursive structure that closely mirrors
+the type deﬁnition
+
+ Later
+sections show how to make the writing of iterative functions more systematic
+by  introducing accumulators  and diﬀerence  lists.   This  lets us  write iterative
+functions from the start.   We ﬁnd that these techniques “scale up”, i.e., they
+work well even for large declarative programs.
+
+A base case.
+A recursive case
+
+fun  {Length  Ls}
+    case  Ls
+    of  nil  then  0
+    []  _|Lr  then  1+{Length  Lr}
+    end
+end
+{Browse  {Length  [a  b  c]}}
+
+type signature: <fun  {$ <List>}: <Int>>
+
+fun  {Append  Ls  Ms}
+    case  Ls
+    of  nil  then  Ms
+    []  X|Lr  then  X|{Append  Lr  Ms}
+    end
+end
+
+fun  {Nth  Xs  N}
+    if  N==1  then  Xs.1
+    elseif  N>1  then  {Nth  Xs.2  N-1}
+    end
+end
+
+The list has only four elements. Trying to ask for the ﬁfth element means trying
+to do Xs.1 or Xs.2 when Xs=nil. This will raise an exception. An exception is
+also raised if N is not a positive integer, e.g., when N=0. This is because there is
+no else clause in the if statement.
+
+Start with a recursive
+deﬁnition of list reversal:
+•  Reverse of nil is nil.
+•  Reverse of X|Xs is Z, where
+reverse of Xs is Ys, and
+append Ys and [X] to get Z.
+
+fun  {Reverse  Xs}
+    case  Xs
+    of  nil  then  nil
+    []  X|Xr  then
+        {Append  {Reverse  Xr}  [X]}
+    end
+end
+
+There will be n recursive calls followed by
+n calls to Append.  Each Append call will have a list of length n/2 on average.
+The total execution time is therefore proportional to n · n/2, namely n2.  This
+is rather slow.   We would expect that reversing a list, which is not exactly a
+complex calculation, would take time proportional to the input length and not
+to its square.
+This program has a second defect:  the stack size grows with the input list
+length,  i.e.,  it  deﬁnes  a recursive  computation  that  is  not iterative.   Naively
+following the recursive deﬁnition of reverse has given us a rather ineﬃcient result!
+
+How can we calculate the list length with an iterative computation, which has
+bounded stack size? To do this, we have to formulate the problem as a sequence
+of state transformations.  That is, we start with a state S0  and we transform it
+successively, giving S1, S2, ..., until we reach the ﬁnal state Sﬁnal, which contains
+the answer.  To calculate the list length, we can take the length i of the part of
+the list already seen as the state. Actually, this is only part of the state. The rest
+of the state is the part Ys of the list not yet seen. The complete state Si  is then
+the pair (i, Ys).  The general intermediate case is as follows for state Si  (where
+the full list Xs is [e1  e2   · · · en])
+
+fun  {IterLength  I  Ys}
+    case  Ys
+    of  nil  then  I
+    []  _|Yr  then  {IterLength  I+1  Yr}
+    end
+end
+
+ Note the diﬀerence with the previous
+deﬁnition. Here the addition I+1 is done before the recursive call to IterLength,
+which is the last call. We have deﬁned an iterative computation.
+
+local
+    fun  {IterLength  I  Ys}
+        case  Ys
+        of  nil  then  I
+        []  _|Yr  then  {IterLength  I+1  Yr}
+        end
+    end
+in
+    fun  {Length  Xs}
+        {IterLength  0  Xs}
+    end
+end
+
+This deﬁnes an iterative computation to calculate the list length.  Note that we
+deﬁne IterLength outside of Length.  This avoids creating a new procedure
+value each time Length is called. There is no advantage to deﬁning IterLength
+inside Length, since it does not use Length’s argument Xs.
+
+
+We can use the same technique on Reverse as we used for Length.  In the
+case of Reverse, the state uses the reverse of the part of the list already seen
+instead of its length.  Updating the state is easy: we just put a new list element
+in front. The initial state is nil. This gives the following version of Reverse:
+
+local
+    fun  {IterReverse  Rs  Ys}
+        case  Ys
+        of  nil  then  Rs
+        []  Y|Yr  then  {IterReverse  Y|Rs  Yr}
+        end
+    end
+in
+    fun  {Reverse  Xs}
+        {IterReverse  nil  Xs}
+    end
+end
+
+This version of Reverse is both a linear-time and an iterative computation.
+
+fun  {LengthL  Xs}
+    case  Xs
+    of  nil  then  0
+    []  X|Xr  andthen  {IsList  X}  then
+        {LengthL  X}+{LengthL  Xr}
+    []  X|Xr  then
+        1+{LengthL  Xr}
+    end
+end
+
+There is an important lesson to be learned here.  It is important to deﬁne a
+recursive type before writing the recursive function that uses it.  Otherwise it is
+easy to be misled by an apparently simple function that is incorrect. This is true
+even in functional languages that do type inference, such as Standard ML and
+Haskell. Type inference can verify that a recursive type is used correctly, but the
+design of a recursive type remains the programmer’s responsibility.
+
+Sorting with mergesort
+
+fun  {Merge  Xs  Ys}
+    case  Xs  #  Ys
+    of  nil  #  Ys  then  Ys
+    []  Xs  #  nil  then  Xs
+    []  (X|Xr)  #  (Y|Yr)  then
+        if  X<Y  then  X|{Merge  Xr  Ys}
+        else  Y|{Merge  Xs  Yr}
+        end
+    end
+end
+
+proc  {Split  Xs  ?Ys  ?Zs}
+    case  Xs
+    of  nil  then  Ys=nil  Zs=nil
+    []  [X]  then  Ys=[X]  Zs=nil
+    []  X1|X2|Xr  then  Yr  Zr  in
+        Ys=X1|Yr
+        Zs=X2|Zr
+        {Split  Xr  Yr  Zr}
+    end
+end
+
+fun  {MergeSort  Xs}
+    case  Xs
+    of  nil  then  nil
+    []  [X]  then  [X]
+    else  Ys  Zs  in
+        {Split  Xs  Ys  Zs}
+        {Merge  {MergeSort  Ys}  {MergeSort  Zs}}
+    end
+end
+
+
+ Accumulators
+  By state threading we mean that each proce-
+dure’s output is the next procedure’s input.  The technique of threading a state
+through nested procedure calls is called accumulator programming.
+
+We no longer program in this style; we ﬁnd that programming with explicit
+state is simpler and more eﬃcient (see Chapter 6). It is reasonable to use a few
+accumulators in a declarative program; it is actually quite rare that a declarative
+program does not need a few. On the other hand, using many is a sign that some
+of them would probably be better written with explicit state.
+
+S#L2={MergeSortAcc  L1  N} takes an input list L1 and an integer N. It
+returns two results: S, the sorted list of the ﬁrst N elements of L1, and L2,
+the remaining elements of L1. The two results are paired together with the
+# tupling constructor.
+
+fun  {MergeSort  Xs}
+    fun  {MergeSortAcc  L1  N}
+        if  N==0  then
+            nil  #  L1
+        elseif  N==1  then
+            [L1.1]  #  L1.2
+        elseif  N>1  then
+            NL=N  div  2
+            NR=N-NL
+            Ys  #  L2  =  {MergeSortAcc  L1  NL}
+            Zs  #  L3  =  {MergeSortAcc  L2  NR}
+        in
+            {Merge  Ys  Zs}  #  L3
+        end
+    end
+in
+    {MergeSortAcc  Xs  {Length  Xs}}.1
+end
+
+The Merge function is unchanged.  Remark that this mergesort does a diﬀerent
+split than the previous one.  In this version, the split separates the ﬁrst half of
+the input list from the second half.  In the previous version, split separates the
+odd-numbered list elements from the even-numbered elements.
+This version has the same time complexity as the previous version. It uses less
+memory because it does not create the two split lists. They are deﬁned implicitly
+by the combination of the accumulating parameter and the number of elements.
+
+Diﬀerence lists
+A diﬀerence list is a pair of two lists, each of which might have an unbound tail.
+The two lists have a special relationship:  it must be possible to get the second
+list from the ﬁrst by removing zero or more elements from the front.
+ The diﬀerence list
+[a  b  c  d]#[d] might contain the lists [a  b  c  d] and [d], but it represents
+neither of these. It represents the list [a  b  c].
+Diﬀerence lists are a special case of diﬀerence structures.  A diﬀerence struc-
+ture is a pair of two partial values where the second value is embedded in the ﬁrst.
+The diﬀerence structure represents a value that is the ﬁrst structure minus the
+second structure.
+Using diﬀerence structures makes it easy to construct iterative
+computations on many recursive datatypes, e.g., lists or trees.  Diﬀerence lists
+and diﬀerence structures are special cases of accumulators in which one of the
+accumulator arguments can be an unbound variable.
+
+The  advantage of using  diﬀerence  lists  is  that when  the  second  list  is an
+unbound variable, another diﬀerence list can be appended to it in constant time.
+To append (a|b|c|X)#X and (d|e|f|Y)#Y, just bind X to (d|e|f|Y).  This
+creates the diﬀerence list (a|b|c|d|e|f|Y)#Y. We have just appended the lists
+[a  b  c] and [d  e  f] with a single binding. 
+
+
+fun  {AppendD  D1  D2}
+    S1#E1=D1
+    S2#E2=D2
+in
+    E1=S2
+    S1#E2
+end
+
+As a concept, a diﬀerence list lives somewhere between the concept of
+value and the concept of state.  It has the good properties of a value (programs
+using them are declarative), but it also has some of the power of state because it
+can be appended once in constant time.
