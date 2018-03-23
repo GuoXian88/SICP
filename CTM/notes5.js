08d0cf5a903c6d6ad2b927ee1ae2e5739f15dc25
xianguo <xianguo@Ctrip.com>
2018-03-23 18:33:18 +0800

    notes

--- CTM/notes5.js
+++ CTM/notes5.js
@@ -72,5 +72,114 @@ sometimes called the source and the consumer is sometimes called the sink. Let
 us look at some pipelines with diﬀerent kinds of transducers.
 
 Filtering a stream
+这个是不是互为质数?不是就是产生2开始的质数流
+
+fun  {Sieve  Xs}
+    case  Xs
+    of  nil  then  nil
+    []  X|Xr  then  Ys  in
+        thread  Ys={Filter  Xr  fun  {$  Y}  Y  mod  X  \=  0  end}  end
+        X|{Sieve  Ys}
+    end
+end
+
+
+local  Xs  Ys  in
+    thread  Xs={Generate  2  100000}  end
+    thread  Ys={Sieve  Xs}  end
+    {Browse  Ys}
+end
+
+Flow control with demand-driven concurrency
+The simplest ﬂow control is called demand-driven concurrency, or lazy execution.
+
+
+proc  {DGenerate  N  Xs}
+    case  Xs  of  X|Xr  then
+        X=N
+        {DGenerate  N+1  Xr}
+    end
+end
+
+fun  {DSum  ?Xs  A  Limit}
+    if  Limit>0  then
+        X|Xr=Xs
+    in
+        {DSum  Xr  A+X  Limit-1}
+    else  A  end
+end
+
+local  Xs  S  in
+    thread  {DGenerate  0  Xs}  end          %  Producer  thread
+    thread  S={DSum  Xs  0  150000}  end  %  Consumer  thread
+    {Browse  S}
+end
+
+Flow control with a bounded buﬀer
+ But
+lazy execution  also has  a serious  problem.   It leads  to a strong  reduction  in
+throughput. By throughput we mean the number of messages that can be sent per
+unit of time.
+ If the consumer
+requests a message, then the producer has to calculate it, and meanwhile the
+consumer waits. If the producer were allowed to get ahead of the consumer, then
+the consumer would not have to wait.
+
+ A
+bounded buﬀer is a transducer that stores elements up to a maximum number, say
+n. The producer is allowed to get ahead of the consumer, but only until the buﬀer
+is full. This limits the extra resource usage to n elements. The consumer can take
+elements from the buﬀer immediately without waiting.  This keeps throughput
+high.   When  the buﬀer has less  than n elements,  the  producer is allowed  to
+produce more elements, until the buﬀer is full.
+
+proc  {Buffer  N  ?Xs  Ys}
+    % Startup从生产者Xs里填充N个元素到buffer中，生产者连续产生N个元素
+    fun  {Startup  N  ?Xs}
+        if  N==0  then  Xs
+        else  Xr  in  Xs=_|Xr  {Startup  N-1  Xr}  end
+    end
+    % 消费者要一个元素就从buffer中取(已经计算好了，无需等待),然后buffer再向生产者请求一个元素
+    proc  {AskLoop  Ys  ?Xs  ?End}
+        case  Ys  of  Y|Yr  then  Xr  End2  in
+            Xs=Y|Xr        %  Get  element  from  buffer
+            End=_|End2  %  Replenish  the  buffer
+            {AskLoop  Yr  Xr  End2}
+        end
+    end
+    End={Startup  N  Xs}
+in
+    {AskLoop  Ys  Xs  End}
+end
+
+Flow control with thread priorities
+
+ Stream objects
+We call a stream object an object because it has an internal state that is
+accessed in a controlled way (by messages on streams). 
+StreamObject is a kind of “template” for creating a stream object. Its behavior
+is deﬁned by NextState, which takes an input message M and a state X1, and
+calculates an output message N and a new state X2. Executing StreamObject in a new thread creates a new stream object with input stream S0, output stream
+T0, and initial state X0. 
+proc  {StreamObject  S1  X1  ?T1}
+    case  S1
+    of  M|S2  then  N  X2  T2  in
+        {NextState  M  X1  N  X2}
+        T1=N|T2
+        {StreamObject  S2  X2  T2}
+    else  skip  end
+end
+
+declare  S0  X0  T0  in
+thread
+    {StreamObject  S0  X0  T0}
+end
+
+
+ Using the declarative concurrent model di-
+rectly
+Order-determining concurrency
+
+
 
  */
