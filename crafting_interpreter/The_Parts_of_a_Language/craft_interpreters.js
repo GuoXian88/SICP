83fda17d3433278bbd357fe941d7167a6486ab2f
xianguo <xianguo@Ctrip.com>
2018-04-17 15:04:10 +0800

    add

--- crafting_interpreter/The_Parts_of_a_Language/craft_interpreters.js
+++ crafting_interpreter/The_Parts_of_a_Language/craft_interpreters.js
@@ -1,5 +1,10 @@
 /**
  I learn best by doing
+ It’s hard for me to wade through paragraphs full of abstract 
+ concepts and really absorb them. But if I’ve coded something, ran it,
+  and debugged it, then I get it.
+
+
  While I intend to show you that a programming language isn’t as daunting as you might believe, it is still a 
  challenge. Rise to it, and you’ll come away a stronger programmer, and smarter about how you use data 
  structures and algorithms in your day job.
@@ -16,21 +21,28 @@ leave you just a tiny bit braver than you were before.
 There are at least six domain-specific languages used in the little 
 system I cobbled together to write and publish this book. What are 
 they?
+这个找不到
+Make cpp YAML Sed Bash CSS HTML?
+
 
 Get a “Hello, world!” program written and running in Java. Set up 
 whatever Makefiles or IDE projects you need to get it working. If you 
 have a debugger, get comfortable with it and step through your 
 program as it runs.
 
+done in IntelliJ IDEA
+
 Do the same thing for C. To get some practice with pointers, define a 
 doubly-linked list of heap-allocated strings. Write functions to 
 insert, find, and delete items from it. Test them.
+
+TODO
 
  */
 
 
  
-
+//Lox/Scanner.java in scanToken() replace 1 line
 default:
     if (isDigit(c)) {
         number();
