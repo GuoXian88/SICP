338bbc30cb15320cef4466eefbfc5810777529d2
BeaverGuo <lalabeaver21@gmail.com>
2018-04-18 06:46:30 +0800

    add

---
+++ scheme/L01-Abstractions-With-Data/ex1-22.scm
@@ -0,0 +1,71 @@
+(define (time-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime))
+)
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime (- (runtime) start-time))
+    )
+)
+
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time)
+)
+
+
+ (define (search-for-primes first last) 
+   (define (search-iter cur last) 
+     (if (<= cur last) (timed-prime-test cur)) 
+     (if (<= cur last) (search-iter (+ cur 2) last))) 
+   (search-iter (if (even? first) (+ first 1) first) 
+                (if (even? last) (- last 1) last))) 
+
+
+;basic operations 
+ (define (square x) 
+     (* x x)) 
+ (define (divides? a b) 
+     (= (remainder b a) 0)) 
+ (define (even? n) 
+   (= (remainder n 2) 0)) 
+ ;smallest divisor computation 
+ (define (smallest-divisor n) 
+     (define (find-divisor n test) 
+       (cond ((> (square test) n) n) 
+             ((divides? test n) test) 
+             (else (find-divisor n (+ test 1))))) 
+     (find-divisor n 2)) 
+ ;primality check 
+ (define (prime? n) 
+     (= n (smallest-divisor n))) 
+ ;check primality of consecutive odd integers in some range 
+ ;time&primality test 
+ ;drRacket has no (runtime) variable; had to substitute it with (current-milliseconds) which is basically same 
+ (define (runtime) 
+     (current-milliseconds)) 
+ (define (timed-prime-test n) 
+     (start-prime-test n (runtime))) 
+ (define (start-prime-test n start-time) 
+     (if (prime? n) 
+         (report-prime n (- (runtime) start-time)) 
+         #f)) 
+ (define (report-prime n elapsed-time) 
+     (display n) 
+     (display "***") 
+     (display elapsed-time) 
+     (newline)) 
+ ;search counter 
+ (define (search-for-primes n counter) 
+   (if (even? n) 
+       (s-f-p (+ n 1) counter) 
+       (s-f-p n counter))) 
+ ;it's important to pay attention to the fact that predicate of the first 'if' here calls (timed-prime-test n) which in case of #t  computes into two procedures - (report-prime n (elapsed-time)) and 'then' case of the first 'if'.  
+ (define (s-f-p n counter) 
+     (if (> counter 0) 
+       (if (timed-prime-test n) 
+           (s-f-p (+ n 2) (- counter 1)) 
+           (s-f-p (+ n 2) counter)) 
+       "COMPUTATION COMPLETE")) 
