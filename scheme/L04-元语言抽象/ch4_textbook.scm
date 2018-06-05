7399b7cef25eb2b9b7a50256872ccb6bbb96c23f
guo <guoxian@evergrande.cn>
2018-06-05 09:30:30 +0800

    add

--- scheme/L04-元语言抽象/ch4_textbook.scm
+++ scheme/L04-元语言抽象/ch4_textbook.scm
@@ -165,5 +165,83 @@ eval <exp> <env>
 
 
 
+(define (make-if predicate consequent alternative)
+    (list 'if predicate consequent alternative)
+)
+
+
+(define (begin? exp)
+    (tagged-list? exp 'begin)
+)
+
+(define (begin-actions exp)
+    (cdr exp)
+)
+
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+    (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))
+    )
+)
+
+(define (make-begin seq) (cons 'begin seq))
+
+
+;过程应用
+(define (application? exp) (pair? exp))
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+
+(define (first-operand ops) (car ops))
+
+(define (rest-operands ops) (cdr ops))
+
+
+;派生表达式 如cond可转化成if
+
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause) (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clauses) (car clauses))
+
+(define (cond-actions clauses) (cdr clauses))
+
+(define (cond->if exp)  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+    (if (null? clauses)
+        'false
+        (let ((first (car clauses))
+                (rest (cdr clauses))
+        )
+            (if (cond-else-clause? rest)
+                (if (null? rest)
+                    (sequence->exp (cond-actions first))
+                    (error "ELSE clause isn't last -- COND->IF" clauses)
+                )
+                (make-if (cond-predicate first)
+                    (sequence->exp (cond-actions first))
+                    (expand-clauses rest)
+                )
+            )
+        )
+    )
+)
+
+
 
 
