ec6306b27183380da6a1aaccc09c7810ae9202d1
guo <guoxian@evergrande.cn>
2018-06-25 16:52:06 +0800

    add

--- crafting_interpreter/The_Parts_of_a_Language/Lox.java
+++ crafting_interpreter/The_Parts_of_a_Language/Lox.java
@@ -8,36 +8,41 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+//函数入口
 public class Lox {
-    static boolean hadError = false;
+  // 内部私有成员指示是否产生错误
+  static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-    } else if (args.length == 1) {
+    } else if (args.length == 1) { // 接收一个参数的时候是通过文件路径读文件执行
       runFile(args[0]);
-    } else {
+    } else {// 没有参数就弹出命令行交互
       runPrompt();
     }
   }
 
   private static void runFile(String path) throws IOException {
-      byte[] bytes = Files.readAllBytes(Paths.get(path));
-      run(new String(bytes, Charset.defaultCharset()));
-      //Indicate an error in the exit code
-      if(hadError) System.exit(65);
+    byte[] bytes = Files.readAllBytes(Paths.get(path)); // 文件路径--> byte
+    run(new String(bytes, Charset.defaultCharset())); // byte --> string encode
+    // Indicate an error in the exit code
+    if (hadError) // 执行错误抛错
+      System.exit(65);
   }
 
   private static void runPrompt() throws IOException {
-    InputStreamReader input = new InputStreamReader(System.in);
-    BufferedReader reader = new BufferedReader(input);
+    InputStreamReader input = new InputStreamReader(System.in);// 输入流
+    BufferedReader reader = new BufferedReader(input);// 输入流-->buffer
 
-    for (;;) { 
+    for (;;) {
       System.out.print("> ");
-      run(reader.readLine());
+      run(reader.readLine());// buffer转string(readLine内部?)
       hadError = false;
     }
   }
 
+  // string-->tokens
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
@@ -47,22 +52,20 @@ public class Lox {
       System.out.println(token);
     }
   }
-  /*分离出错和报错
-  Ideally, we would have an actual abstraction, some kind of “ErrorReporter” interface that gets passed to the scanner and 
-  parser so that we can swap out different reporting strategies.
 
-  */
+  /*
+   * 分离出错的代码和报错的代码  TODO: Ideally, we would have an actual abstraction, some k
+   * nd of “ErrorReporter” interface that gets passed to the scanner and 
+   * arser  o that we can swap out different reporting strategies.
+   * 
+   */
   static void error(int line, String message) {
-      report(line, "", message);
+    report(line, "", message);
   }
 
-
   private static void report(int line, String where, String message) {
-      System.err.println(
-          "[line " + line + "] Error" + where + ": " + message
-      );
-      hadError = true;
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
   }
 
 }
-
--- crafting_interpreter/The_Parts_of_a_Language/Scanner.java
+++ crafting_interpreter/The_Parts_of_a_Language/Scanner.java
@@ -1,12 +1,13 @@
-package com.craftinginterpreters.lox;
+package com.craftinginterpreters.lox; //这个可能是命名空间
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.craftinginterpreters.lox.TokenType.*; 
+import static com.craftinginterpreters.lox.TokenType.*;
 
+//想起了stream
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
@@ -14,6 +15,28 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -33,64 +56,110 @@ class Scanner {
   private void scanToken() {
     char c = advance();
     switch (c) {
-      case '(': addToken(LEFT_PAREN); break;
-      case ')': addToken(RIGHT_PAREN); break;
-      case '{': addToken(LEFT_BRACE); break;
-      case '}': addToken(RIGHT_BRACE); break;
-      case ',': addToken(COMMA); break;
-      case '.': addToken(DOT); break;
-      case '-': addToken(MINUS); break;
-      case '+': addToken(PLUS); break;
-      case ';': addToken(SEMICOLON); break;
-      case '*': addToken(STAR); break;
+    case '(':
+      addToken(LEFT_PAREN);
+      break;
+    case ')':
+      addToken(RIGHT_PAREN);
+      break;
+    case '{':
+      addToken(LEFT_BRACE);
+      break;
+    case '}':
+      addToken(RIGHT_BRACE);
+      break;
+    case ',':
+      addToken(COMMA);
+      break;
+    case '.':
+      addToken(DOT);
+      break;
+    case '-':
+      addToken(MINUS);
+      break;
+    case '+':
+      addToken(PLUS);
+      break;
+    case ';':
+      addToken(SEMICOLON);
+      break;
+    case '*':
+      addToken(STAR);
+      break;
 
-      case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
-      case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
-      case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
-      case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+    case '!':
+      addToken(match('=') ? BANG_EQUAL : BANG);
+      break;
+    case '=':
+      addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+      break;
+    case '<':
+      addToken(match('=') ? LESS_EQUAL : LESS);
+      break;
+    case '>':
+      addToken(match('=') ? GREATER_EQUAL : GREATER);
+      break;
 
-      case '/':
-        if (match('/')) {
-          // A comment goes until the end of the line.
-          while (peek() != '\n' && !isAtEnd()) advance();
-        } else {
-          addToken(SLASH);
-        }
-        break;
+    case '/':
+      if (match('/')) {
+        // A comment goes until the end of the line.
+        while (peek() != '\n' && !isAtEnd())
+          advance();
+      } else {
+        addToken(SLASH);
+      }
+      break;
     case ' ':
     case '\r':
     case '\t':
-        // Ignore whitespace.
-        break;
+      // Ignore whitespace.
+      break;
 
     case '\n':
-        line++;
-        break;
-    case '"': string(); break;
-      default:
-          Lox.error(line, "Unexpected character.");
-        break;
+      line++;
+      break;
+    case '"':
+      string();
+      break;
+    case 'o':
+      // whichever one matches the most characters wins
+      if (peek() == 'r') {
+        addToken(OR);
+      }
+      break;
+    default:
+      if (isDigit(c)) {
+        number();
+      } else if (isAlpha(c)) {
+        identifier();
+      } else {
+        // static method
+        Lox.error(line, "Unexpected character.");
+      }
+      break;
 
     }
   }
 
   private boolean match(char expected) {
-    if (isAtEnd()) return false;
-    if (source.charAt(current) != expected) return false;
+    if (isAtEnd())
+      return false;
+    if (source.charAt(current) != expected)
+      return false;
 
     current++;
     return true;
   }
 
   private char peek() {
-    if (isAtEnd()) return '\0';
+    if (isAtEnd())
+      return '\0';
     return source.charAt(current);
   }
 
-
-  //helper fn
+  // helper fn
   private boolean isAtEnd() {
-      return current >= source.length();
+    return current >= source.length();
   }
 
   private char advance() {
@@ -98,7 +167,9 @@ class Scanner {
     return source.charAt(current - 1);
   }
 
+  // why overload?
   private void addToken(TokenType type) {
+    // 根据参数自动匹配
     addToken(type, null);
   }
 
@@ -107,8 +178,51 @@ class Scanner {
     tokens.add(new Token(type, text, literal, line));
   }
 
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
 
+  private void number() {
+    while (isDigit(peek()))
+      advance();
 
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
 
+      while (isDigit(peek()))
+        advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek()))
+      advance();
+
+    // See if the identifier is a reserved word.
+    String text = source.substring(start, current);
+
+    TokenType type = keywords.get(text);
+    if (type == null)
+      type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
 
 }
