e1ace1bc3845b55bc70913dc5f017e2930a16b6c
guo <guoxian@evergrande.cn>
2018-06-29 16:24:34 +0800

    add

--- crafting_interpreter/Parsing_expressions/Parser.java
+++ crafting_interpreter/Parsing_expressions/Parser.java
@@ -2,9 +2,12 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
-import static com.craftinginterpreters.lox.TokenType.*;
+import static com.craftinginterpreters.lox.TokenType.*; //还能这样导入
 
 class Parser {
+    private static class ParseError extends RuntimeException {
+    }
+
     private final List<Token> tokens;
     private int current = 0;
 
@@ -12,6 +15,17 @@ class Parser {
         this.tokens = tokens;
     }
 
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
+    // Each method for parsing a grammar rule produces a syntax tree for that rule
+    // and returns it to the caller. When the body of the rule contains a
+    // nonterminal—a reference to another rule—we call that rule’s method.
     private Expr expression() {
         return equality();
     }
@@ -22,6 +36,7 @@ class Parser {
         // Let’s step through it. The left comparison nonterminal in the body is
         // translated to the first call to comparison() and we store that in a local
         // variable.
+        // comparison会解析左或者右边的expression
         Expr expr = comparison();
 
         // Then, the ( ... )* loop in the rule is mapped to a while loop. We need to
@@ -34,8 +49,154 @@ class Parser {
             Expr right = comparison();
             expr = new Expr.Binary(expr, operator, right);
         }
+        // 返回语法树
+        return expr;
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type))
+            return advance();
+
+        throw error(peek(), message);
+    }
+
+    private boolean check(TokenType tokenType) {
+        if (isAtEnd())
+            return false;
+        return peek().type == tokenType;
+    }
+
+    private Token advance() {
+        if (!isAtEnd())
+            current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON)
+                return;
+
+            switch (peek().type) {
+            case CLASS:
+            case FUN:
+            case VAR:
+            case FOR:
+            case IF:
+            case WHILE:
+            case PRINT:
+            case RETURN:
+                return;
+            }
+
+            advance();
+        }
+    }
+
+    // comparison → addition ( ( ">" | ">=" | "<" | "<=" ) addition )* ;
+
+    private Expr comparison() {
+        Expr expr = addition();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = addition();
+            expr = new Expr.Binary(expr, operator, right);
+        }
 
         return expr;
+    }
+
+    private Expr addition() {
+        Expr expr = multiplication();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = multiplication();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr multiplication() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    // unary → ( "!" | "-" ) unary
+    // | primary ;
+    private Expr unary() {
+        // If it’s a ! or -, we must have a unary expression. In that case, we grab the
+        // token, and then recursively call unary() again to the parse the operand. Wrap
+        // that all up in a unary expression syntax tree and we’re done.
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    // primary → NUMBER | STRING | "false" | "true" | "nil"
+    // |"("expression")";
+
+    private Expr primary() {
+        if (match(FALSE))
+            return new Expr.Literal(false);
+        if (match(TRUE))
+            return new Expr.Literal(true);
+        if (match(NIL))
+            return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+        throw error(peek(), "Expect expression.");
     }
 
 }
--- crafting_interpreter/Parsing_expressions/index.js
+++ crafting_interpreter/Parsing_expressions/index.js
@@ -1,6 +1,36 @@
 /**
  * 
  * 
+ * 
+ * 随机选一个breakfast， with这种引号的是token
+breakfast → protein "with" bread ;
+breakfast → protein ;
+breakfast → bread ;
+可以递归所以可以compose
+protein   → protein "and" protein ;
+protein   → "bacon" ;
+protein   → "sausage" ;
+protein   → cooked "eggs" ;
+
+cooked    → "scrambled" ;
+cooked    → "poached" ;
+cooked    → "fried" ;
+
+bread     → "toast" ;
+bread     → "biscuits" ;
+bread     → "English muffin" ;
+
+
+进化版
+breakfast → protein ( "and" protein )* ( "with" bread )?
+          | bread ;
+
+protein   → "bacon"
+          | "sausage"
+          | ( "scrambled" | "poached" | "fried" ) "eggs" ;
+
+bread     → "toast" | "biscuits" | "English muffin" ;
+
  expression → literal
            | unary
            | binary
